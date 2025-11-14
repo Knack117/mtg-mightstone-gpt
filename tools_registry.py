0351146d5e71747cdc17f74df7dbb3cb86c2b30e
Knack117 <134350188+Knack117@users.noreply.github.com>
2025-11-13 22:50:03 -0500

    Add budget comparison tool to registry

--- tools_registry.py
+++ tools_registry.py
@@ -1,5 +1,7 @@
 from handlers.edhrec_average_deck import edhrec_average_deck
+from handlers.edhrec_budget import edhrec_budget_comparison
 
 TOOL_REGISTRY = {
     "edhrec_average_deck": edhrec_average_deck,
+    "edhrec_budget_comparison": edhrec_budget_comparison,
 }
